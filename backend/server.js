cff235407a2c37c69a06d25dbb3c6eea9f6f89ff
Saim Ch <saimch726@gmail.com>
2026-05-25 00:16:45 +0500

    Setup server

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('API is Working...')
+});
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
